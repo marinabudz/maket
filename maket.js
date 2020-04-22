aa3c88f6e17875ab708dc171f117eaa8917b5f8c
marina <budz.marina97@gmail.com>
2020-04-22 12:40:58 +0300

    upload
    upload

---
+++ maket.js
@@ -0,0 +1,217 @@
+var adata = new Vue({
+    el: '#adata',
+    data: {
+        header_menu: [{
+                url: "",
+                name: "home"
+
+            },
+            {
+                url: "",
+                name: "about"
+
+            },
+            {
+                url: "",
+                name: "expertise"
+
+            },
+            {
+                url: "",
+                name: "teams"
+
+            },
+            {
+                url: "",
+                name: "works"
+
+            },
+            {
+                url: "",
+                name: "people say"
+
+            },
+            {
+                url: "",
+                name: "contact"
+
+            },
+
+        ],
+
+    }
+
+})
+
+var expertiseGrid = new Vue({
+    el: '#expertiseGrid',
+    data: {
+        expertise_grid: [{
+                image: "photos/forthBlock/1.png",
+                title: "WEB DESIGN & DEVELOPMENT",
+                description: `This is Photoshop's version  of Lorem 
+            Ipsum. Proin gravida nibh vel velit
+            auctor aliquet Aenean.`
+            },
+            {
+                image: "photos/forthBlock/4.png",
+                title: "SEARCH ENGINE OPTIMIZATION",
+                description: `This is Photoshop's version  of Lorem 
+            Ipsum. Proin gravida nibh vel velit
+            auctor aliquet Aenean.`
+            },
+            {
+                image: "photos/forthBlock/3.png",
+                title: "MOBILE APP",
+                description: `This is Photoshop's version  of Lorem 
+            Ipsum. Proin gravida nibh vel velit
+            auctor aliquet Aenean.`
+            },
+            {
+                image: "photos/forthBlock/2.png",
+                title: "BRANDING IDENTITY",
+                description: `This is Photoshop's version  of Lorem 
+            Ipsum. Proin gravida nibh vel velit
+            auctor aliquet Aenean.`
+            },
+
+            {
+                image: "photos/forthBlock/5.png",
+                title: "GAME DEVELOPMENT",
+                description: `This is Photoshop's version  of Lorem 
+            Ipsum. Proin gravida nibh vel velit
+            auctor aliquet Aenean.`
+            },
+            {
+                image: "photos/forthBlock/6.png",
+                title: "MADE WITH LOVE",
+                description: `This is Photoshop's version  of Lorem 
+            Ipsum. Proin gravida nibh vel velit
+            auctor aliquet Aenean.`
+            },
+        ],
+
+    }
+})
+var teamGrid = new Vue({
+    el: '#teamGrid',
+    data: {
+        team_grid: [{
+                image: "photos/fifthBlock/2.jpg",
+                title: "semf ucuk",
+                description: `ceo & founder`
+            },
+
+            {
+                image: "photos/fifthBlock/1.jpg",
+                title: "dik adalin",
+                description: `engineering`
+            },
+
+            {
+                image: "photos/fifthBlock/4.jpg",
+                title: "jeng kol",
+                description: `designer`
+            },
+
+            {
+                image: "photos/fifthBlock/3.jpg",
+                title: "pet romak",
+                description: `marketing`
+            },
+
+
+
+        ],
+
+    }
+})
+
+var worksGrid = new Vue({
+    el: '#worksGrid',
+    data: {
+        works_grid: [{
+                image: "photos/sixthBlock/1.jpg",
+            },
+            {
+                image: "photos/sixthBlock/2.jpg",
+            },
+            {
+                image: "photos/sixthBlock/7.jpg",
+            },
+            {
+                image: "photos/sixthBlock/4.jpg",
+            },
+            {
+                image: "photos/sixthBlock/5.jpg",
+            },
+            {
+                image: "photos/sixthBlock/6.jpg",
+            },
+        ],
+    }
+})
+
+
+var contactGrid = new Vue({
+    el: '#contactGrid',
+    data: {
+        contact_grid: [{
+                image: "photos/eightBlock/1.png",
+            },
+            {
+                image: "photos/eightBlock/2.png",
+            },
+            {
+                image: "photos/eightBlock/3.png",
+            },
+            {
+                image: "photos/eightBlock/4.png",
+            },
+            {
+                image: "photos/eightBlock/5.png",
+            },
+            {
+                image: "photos/eightBlock/6.png",
+            },
+            {
+                image: "photos/eightBlock/7.png",
+            },
+            {
+                image: "photos/eightBlock/8.png",
+            },
+            {
+                image: "photos/eightBlock/9.png",
+            },
+            {
+                image: "photos/eightBlock/10.png",
+            },
+
+        ],
+    }
+})
+
+var networksGrid = new Vue({
+    el: '#networksGrid',
+    data: {
+        networks_grid: [{
+                image: "photos/ninthBlock/1.png",
+            },
+            {
+                image: "photos/ninthBlock/2.png",
+            },
+            {
+                image: "photos/ninthBlock/3.png",
+            },
+            {
+                image: "photos/ninthBlock/4.png",
+            },
+            {
+                image: "photos/ninthBlock/5.png",
+            },
+            {
+                image: "photos/ninthBlock/6.png",
+            },
+        ],
+    }
+})
